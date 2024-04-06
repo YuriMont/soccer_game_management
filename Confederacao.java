f4a455f4b8d72aa6b7a7bc072195f74dd20d9529
Luan H. Medeiros <139378326+luanleprechaun@users.noreply.github.com>
2024-04-06 19:35:00 -0300

    Add files via upload
    fiz classe ai

---
+++ Confederacao.java
@@ -0,0 +1,28 @@
+public class Confederacao {
+    private String pais;
+    private Liga liga;
+    private Copa copa;
+
+    Confederacao(){}
+    
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    public Liga getLiga() {
+        return liga;
+    }
+    public void setLiga(Liga liga) {
+        this.liga = liga;
+    }
+    public Copa getCopa() {
+        return copa;
+    }
+    public void setCopa(Copa copa) {
+        this.copa = copa;
+    }
+
+    
+}
